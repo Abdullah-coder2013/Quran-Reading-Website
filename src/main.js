cbb711a05f24466561c9600930a18a88e618748a
Abdullah-coder2013 <93675484+Abdullah-coder2013@users.noreply.github.com>
2023-02-27 17:55:58 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+var response = "";
+
+  document.getElementById("selectbtn").addEventListener("click", function(e) {
+    async function get() {
+      let data = document.getElementById("surahs").value;
+
+      res = await fetch(`http://api.alquran.cloud/v1/surah/${data}`);
+      return await res.json();
+    }
+    let apiresponse = get().then(async res => {
+      res["data"]["ayahs"].forEach(element => {
+        document.getElementById("display").value += `${element.text}\n`;
+      });
+    })
+  });
+
